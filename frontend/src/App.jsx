f0dbd69753d86932efe0e788811eb3412fc44a73
Daniel <105226307+Thoobi@users.noreply.github.com>
2024-05-07 14:42:58 +0100

    intial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+const App = () => {
+  console.log("Welcome to FindmySauce");
+  return (
+    <div>
+      <h1>Welcome to find my sauce</h1>
+      <p>A project by Ola, Daniel, Mo</p>
+    </div>
+  )
+}
+
+export default App
